031b9a194895f068d11ea79b75a6df5100b20949
DilverSingh23 <dsinghh23@gmail.com>
2025-04-03 22:00:15 -0400

    feat: add agenda planner component functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import AgendaPlanner from './components/AgendaPlanner'
 
+
 const App = () => {
+  const [userInput, setUserInput] = useState('')
   return (
     <main>
       <div className='flex h-screen bg-[url(./citybackground.gif)] bg-cover bg-center justify-center'>
         <div className='flex flex-col items-center gap-10  '>
           <Header/>
-          <AgendaPlanner/>
+          <AgendaPlanner userInput={userInput} setUserInput={setUserInput}/>
+          <p className='text-white'>{userInput}</p>
         </div>
       </div>
     </main>
--- src/components/AgendaPlanner.jsx
+++ src/components/AgendaPlanner.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 
-const AgendaPlanner = () => {
+
+const AgendaPlanner = ({userInput, setUserInput}) => {
+    const checkUserInput = (event) => {
+        event.preventDefault();
+        const userInputValue = document.getElementById("user-input").value
+        const form = document.getElementById("form")
+        if (userInputValue.length == 0) {
+            setUserInput('')
+            alert('Cannot create an empty task, please enter a character.')
+            console.log(userInput)
+        }
+        else if (userInputValue.length > 75) {
+            setUserInput('')
+            alert('Input is too long.')
+            console.log(userInput)
+
+        }
+        else {
+            setUserInput(userInputValue)
+            form.reset()
+            console.log(userInput)
+        }
+    }
     return (
         <div className='flex justify-center h-150 w-225 bg-black rounded-4xl'>
-            <input className='mt-10 h-10 w-200 bg-white rounded-4xl'
-                type="text"
-                placeholder='   Enter the task and deadline'
-            >
-
-            </input>
+            <form onSubmit={checkUserInput} id = "form"> 
+                <input className='mt-10 h-10 w-200 bg-white rounded-4xl p-5'
+                    type="text"
+                    id="user-input"
+                    placeholder='Enter the task and deadline'
+                >
+                </input> 
+            </form>
         </div>
     )
 }
