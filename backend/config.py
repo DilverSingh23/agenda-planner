6f3c227a33f18c4f22a2b5d320f2afc09fee57c6
DilverSingh23 <dsinghh23@gmail.com>
2025-07-19 20:20:31 -0400

    set up flask app and database configurations

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,12 @@
 # Contains the main configurations of our applications
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy # Allows us to interact with relational databases (SQLite, PostgresSQL, MySQL)
+from flask_cors import CORS # CORS: Cross Origin Requests | Allows our frontend to make requests to the backend server
+
+app = Flask(__name__) # Initializes the Flask app
+CORS(app) # Allows us to make cross origin requests between our frontend and backend in our app
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db" # Configures what relational database our Flask app is interacting with (SQLite in this case)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # We are disabling SQLAlchemy's event system that tracks object modifications, as we'll handle database updates manually with db.session.commit()
+
+db = SQLAlchemy(app) # Initializes the database instance using our app's SQLite configuration. This gives us access to SQLAlchemy's ORM (Object-Relational Mapping) for performing CRUD operations in Python, which are translated into SQL under the hood.
